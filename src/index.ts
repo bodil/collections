06456b3d5aad332c13ecc769d371a725caa5f226
Bodil Stokke <17880+bodil@users.noreply.github.com>
2025-02-28 14:48:02 +0000

    Removed the extraneous ordered map/set types.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { LinkedList, type ListNode } from "./linked-list";
 export { LFUCache } from "./lfu-cache";
 export { LRUCache } from "./lru-cache";
-export { OrderedMap } from "./ordered-map";
-export { OrderedSet } from "./ordered-set";
